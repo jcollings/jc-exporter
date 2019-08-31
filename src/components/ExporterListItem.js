17a080752cfaef88ccf918695b8ce041ff92fd09
James Collings <james@jclabs.co.uk>
2019-08-31 11:07:16 +0100

    Update exporter list item display

--- src/components/ExporterListItem.js
+++ src/components/ExporterListItem.js
@@ -11,12 +11,21 @@ export default class ExporterListItem extends React.Component {
 
         return (
             <div className="ewp-exporter-list__item">
-                <h1>{name}</h1>
-                <p>Export <strong>{type}</strong> to <strong>{file_type}</strong> file.</p>
-                <div className="ewp-buttons">
-                    <Link to={AJAX_BASE+'&edit=' + id } className="button button-secondary button-small">Edit</Link>
-                    <a onClick={() => { this.props.onRun(this.props.exporter); }} className="button button-primary button-small">Export</a>
-                    <a onClick={() => { this.props.onDelete(this.props.exporter); }} className="button button-link-delete button-small">Delete</a>
+                <div className="ewp-item">
+                    <div className="ewp-item__left">
+                        <h2>{name}</h2>
+                        <p>Export <strong>{type}</strong> to <strong>{file_type}</strong> file.</p>
+                    </div>
+                    <div className="ewp-item__right">
+                        <div className="ewp-buttons">
+                            <Link to={AJAX_BASE+'&edit=' + id } className="button button-secondary button-small">Edit</Link>
+                            <a onClick={() => { this.props.onRun(this.props.exporter); }} className="button button-primary button-small">Export</a>
+                            <a onClick={() => { this.props.onDelete(this.props.exporter); }} className="button button-link-delete button-small">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="ewp-item__progress">
+                    <p>Last Exported on 31/08/2019</p>
                 </div>
             </div>
         );
